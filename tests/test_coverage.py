e490a50d229bd2acbe5a36d0ebbbade965f798c4
Denis Sergeev <zeff@altlinux.org>
2024-12-19 17:52:49 +0300

    feat: add new tests to test_coverage.py

--- tests/test_coverage.py
+++ tests/test_coverage.py
@@ -4,7 +4,16 @@ from core.coverage import Coverage
 from helper.cgi_decode import cgi_decode
 
 class TestCoverage(unittest.TestCase):
-    def test_cov(self):
+    
+    def test_cov_diff(self):
+        with Coverage() as cov1:
+            cgi_decode("a+b")
+        with Coverage() as cov2:
+            cgi_decode("a b")
+        self.assertTrue(len(cov1.coverage() - cov2.coverage()) > 0)
+        self.assertTrue(len(cov2.coverage() - cov1.coverage()) == 0)
+ 
+    def test_cov_basic(self):
         before = 0
         middle = 0
         after = 0
